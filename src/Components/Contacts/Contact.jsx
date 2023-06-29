78a34c7aef73bbe26c32ecf55ab2a3e4bf8382fe
HuseinliAli <ali.guseynoov@gmail.com>
2023-06-29 23:08:28 +0400

    localStorage is solved

--- src/Components/Contacts/Contact.jsx
+++ src/Components/Contacts/Contact.jsx
@@ -3,16 +3,19 @@ import List from "./List/List";
 import Form from "./Form/Form";
 const Contact = () => {
   const [contacts, setContacts] = useState([]);
-
+  const [contactsData, setContactsData] = useState([]);
   useEffect(() => {
-    console.log(contacts);
-    setContacts(contacts);
+    if (contacts.length !== 0) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+    const json = JSON.parse(localStorage.getItem("contacts"));
+    setContactsData(json);
   }, [contacts]);
 
   return (
     <div>
-      <List contactList={contacts} />
       <Form addContact={setContacts} contacts={contacts} />
+      <List contactList={contactsData ? contactsData : contacts} />
     </div>
   );
 };
--- src/Components/Contacts/Form/Form.jsx
+++ src/Components/Contacts/Form/Form.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Form = ({ addContact, contacts }) => {
   const [form, setForm] = useState({ name: "", phone: "" });
+
   const onChangeInput = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
     if (form.name === "" || form.phone === "") {
       return false;
     }
     addContact([...contacts, form]);
-    console.log(form);
   };
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem("contacts");
+    if (storedContacts) {
+      addContact(JSON.parse(storedContacts));
+    }
+  }, []);
   return (
     <form onSubmit={onSubmit}>
       <div>
