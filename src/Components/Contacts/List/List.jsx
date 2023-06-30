265988d7b4aa3509ecfcf316e2f6ddbbe50880bb
HuseinliAli <ali.guseynoov@gmail.com>
2023-06-30 16:17:46 +0400

    seacrh is ready

--- src/Components/Contacts/List/List.jsx
+++ src/Components/Contacts/List/List.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ contactList }) => {
+  const [filteredText, setFilteredText] = useState("");
+
+  const filtered = contactList.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toLowerCase()
+        .includes(filteredText.toLocaleLowerCase())
+    );
+  });
   return (
     <div>
       <h2>Contact List</h2>
-      {contactList.map((contact, index) => (
+      <input
+        placeholder="Seacrh contact"
+        type="text"
+        value={filteredText}
+        onChange={(e) => {
+          setFilteredText(e.target.value);
+        }}
+      />
+      {filtered.map((contact, index) => (
         <p key={index}>
           {contact.name}: {contact.phone}
         </p>
